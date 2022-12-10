e9c5766bfbbf029111d46b29d4c052e8d6f55dca
tpoff6 <tpoff6@gatech.edu>
2022-12-09 18:33:17 -0600

    added more tests and completed initial nlp pipeline for subject and intent classification.

--- Python/NLP_Pipeline/NLP_Pipeline.py
+++ Python/NLP_Pipeline/NLP_Pipeline.py
@@ -12,13 +12,56 @@ class NLP_Pipeline:
                                                      model="Jean-Baptiste/roberta-large-ner-english", device=device)
         self.zero_shot_classifier_pipeline = pipeline("zero-shot-classification",
                                                       model="facebook/bart-large-mnli", device=device)
+        self.previous_subject = ""
 
     def nlp_get_subject(self, text_input):
-        global subject
         # first get all named entities from the text input
         entities = self.named_entity_recognition_pipeline(text_input)
         # get all location entities
         location_entities = [entity for entity in entities if entity['entity_group'] == 'LOC']
-        subject = location_entities[0]['word'] if len(
-            location_entities) != 0 else subject  # replace subject is there is a new location entity
+        subject = location_entities[0]['word'].strip() if len(
+            location_entities) != 0 else None  # replace subject is there is a new location entity
+
+
         return subject, entities
+
+    def nlp_get_major_category(self, text_input):
+        # major categories, text info, videos, images, tweets,
+        major_categories = ["videos", "images", "tweets", "info"]
+        classes = self.zero_shot_classifier_pipeline(text_input, candidate_labels=major_categories)
+        major_category = classes['labels'][0]
+        major_category_score = classes['scores'][0]
+        major_category = major_category.strip()
+        return major_category, major_category_score
+    def nlp_get_info_category_strat_multi_point_category(self, text_input):
+        history_categories = ["history"]
+        tourist_categories = ["tourist", "eat"]
+        general_info_categories = ["general"]
+        info_categories = history_categories + tourist_categories + general_info_categories
+        classes = self.zero_shot_classifier_pipeline(text_input, candidate_labels=info_categories)
+        major_category = classes['labels'][0]
+        major_category_score = classes['scores'][0]
+        average_distance = sum([major_category_score - i for i in classes['scores'][1:]]) / (len(classes['scores']))
+        if major_category in history_categories:
+            major_category = "history"
+        if major_category in tourist_categories:
+            major_category = "tourist"
+        if major_category in general_info_categories:
+            major_category = "general"
+        return major_category, major_category_score
+
+    def pipeline(self, text_input):
+        # first get all named entities from the text input
+        subject, entities = self.nlp_get_subject(text_input)
+        if subject == None:
+            subject = self.previous_subject
+        self.previous_subject = subject
+        text_input = text_input.replace(subject.lower(), subject.capitalize())
+        # next, we need to figure out what the user is asking for
+        major_category, major_category_score = self.nlp_get_major_category(text_input)
+
+        # next, if we have a request for info, we need to break down what info they are asking for,
+        if major_category == "info":
+            major_category, major_category_score = self.nlp_get_info_category_strat_multi_point_category(text_input)
+
+        return subject, major_category
--- Python/tests/NLP_Pipeline_Tests.py
+++ Python/tests/NLP_Pipeline_Tests.py
@@ -10,6 +10,98 @@ class MyTestCase(unittest.TestCase):
 
         self.assertEqual(predicted_subject, "Dallas Texas")
 
+    def test_major_subject_extraction(self):
+        sentences = [
+            ("show me pics of downtown Dallas.", "images"),
+            ("show me Dallas videos.", "videos"),
+            ("tell me about Dallas.", "info"),
+            ("general info about Dallas.", "info"),
+            ("what is Dallas.", "info"),
+            ("what is the history of Dallas.", "info"),
+            ("when was Dallas Founded.", "info"),
+            ("tell me about Dallas's past.", "info"),
+            ("When was the last slave Freed in Texas?", "info"),
+            ("Things to do in Dallas", "info"),
+            ("site seeing Dallas", "info"),
+            ("Places to go Dallas", "info"),
+            ("Places to eat Dallas", "info"),
+            ("Dallas nightlife", "info"),
+            ("5 start hotels Dallas", "info"),
+            ("Dallas housing", "info"),
+        ]
+        pipeline = NLP_Pipeline()
+        for sentence, major_class in sentences:
+            predicted_class, _ = pipeline.nlp_get_major_category(sentence)
+            self.assertEqual(predicted_class, major_class)
+
+    def test_info_sub_subject_extraction(self):
+        sentences = [
+            ("tell me about Dallas.", "general"),
+            ("general info about Dallas.", "general"),
+            ("what is Dallas.", "general"),
+            ("what is the history of Dallas.", "history"),
+            ("when was Dallas Founded.", "history"),
+            ("tell me about Dallas's past.", "history"),
+            ("When was the last slave Freed in Texas?", "history"),
+            ("Things to do in Dallas", "tourist"),
+            ("site seeing Dallas", "tourist"),
+            ("Places to go Dallas", "tourist"),
+            ("Places to eat Dallas", "tourist"),
+            ("Dallas nightlife", "tourist"),
+            ("5 start hotels Dallas", "tourist"),
+            ("Dallas housing", "tourist"),
+        ]
+        pipeline = NLP_Pipeline()
+        for sentence, major_class in sentences:
+            predicted_class, _ = pipeline.nlp_get_info_category_strat_multi_point_category(sentence)
+            self.assertEqual(predicted_class, major_class)
+    def test_pipeline_subject_intent_extraction(self):
+        sentences = [
+            ("tell me about Dallas.", "general", "Dallas"),
+            ("general info about Dallas.", "general", "Dallas"),
+            ("what is Dallas.", "general", "Dallas"),
+            ("what is the history of Dallas.", "history", "Dallas"),
+            ("when was Dallas founded.", "history", "Dallas"),
+            ("tell me about Dallas past.", "history", "Dallas"),
+            ("When was the last slave Freed in Texas?", "history", "Texas"),
+            ("Things to do in Dallas", "tourist", "Dallas"),
+            ("site seeing Dallas", "tourist", "Dallas"),
+            ("Places to go Dallas", "tourist", "Dallas"),
+            ("Places to eat Dallas", "tourist", "Dallas"),
+            ("Dallas nightlife", "tourist", "Dallas"),
+            ("5 start hotels Dallas", "tourist", "Dallas"),
+            ("Dallas housing", "tourist", "Dallas"),
+        ]
+        pipeline = NLP_Pipeline()
+        for sentence, major_class, subject in sentences:
+            predicted_subject, predicted_class = pipeline.pipeline(sentence)
+            self.assertEqual(predicted_class, major_class)
+            self.assertEqual(predicted_subject, subject)
+    def test_pipeline_subject_intent_extraction_lowercase(self):
+        sentences = [
+            ("tell me about Dallas.", "general", "Dallas"),
+            ("general info about Dallas.", "general", "Dallas"),
+            ("what is Dallas.", "general", "Dallas"),
+            ("what is the history of Dallas.", "history", "Dallas"),
+            ("when was Dallas founded.", "history", "Dallas"),
+            ("tell me about Dallas past.", "history", "Dallas"),
+            ("When was the last slave Freed in Texas?", "history", "Texas"),
+            ("Things to do in Dallas", "tourist", "Dallas"),
+            ("site seeing Dallas", "tourist", "Dallas"),
+            ("Places to go Dallas", "tourist", "Dallas"),
+            ("Places to eat Dallas", "tourist", "Dallas"),
+            ("Dallas nightlife", "tourist", "Dallas"),
+            ("5 start hotels Dallas", "tourist", "Dallas"),
+            ("Dallas housing", "tourist", "Dallas"),
+        ]
+        pipeline = NLP_Pipeline()
+        for sentence, major_class, subject in sentences:
+            sentence = sentence.lower()
+            subject = subject.lower()
+            predicted_subject, predicted_class = pipeline.pipeline(sentence)
+            self.assertEqual(predicted_class, major_class)
+            self.assertEqual(predicted_subject, subject)
+
 
 
 if __name__ == '__main__':
