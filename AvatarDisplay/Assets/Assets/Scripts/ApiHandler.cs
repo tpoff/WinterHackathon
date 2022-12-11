43fd9fcbb974bf81abe6c9ec00558cb3877a227b
tpoff6 <tpoff6@gatech.edu>
2022-12-11 04:57:15 -0600

    added splash page and audio to avatar project, added code to ping api and get bot status, bot will wait for splash audio to conclude before trying to ping the api.

--- AvatarDisplay/Assets/Assets/Scripts/ApiHandler.cs
+++ AvatarDisplay/Assets/Assets/Scripts/ApiHandler.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Net;
+using System;
+using System.IO;
+using UnityEngine.Networking;
 
+[Serializable]
+public class BotStatus
+{
+    public string lastRoundStatus;
+    public float lastMessageTime;
+    public string lastMessage;
+    public string partialMessage;
+    public string botLoopStep;
+    public string subject;
+    public string category;
+}
 public class ApiHandler : MonoBehaviour
 {
-    public string generatedPagePath = "";
-    public string generatedAudioPath = "";
-    public string initialPagePath = "";
-    public string initialAudioPath = "";
+    public string api_endpoint = "http://127.0.0.1:5000/api/status";
+
+    public string generatedPagePath = "C:\\Users\\Tyler\\Desktop\\Hackathon\\WinterHackathon\\BotContent\\html_result.html";
+    public string generatedAudioPath = "C:\\Users\\Tyler\\Desktop\\Hackathon\\WinterHackathon\\BotContent\\tts_response.wav";
+    public string initialPagePath = "C:\\Users\\Tyler\\Desktop\\Hackathon\\WinterHackathon\\BotContent\\SplashPage.html";
+    public string initialAudioPath = "C:\\Users\\Tyler\\Desktop\\Hackathon\\WinterHackathon\\BotContent\\splash_audio.wav";
     public AvatarAnimatorScript animatorScript;
     public DynamicPageLoader pageLoader;
     public PulseEmissionFromSound emissionController;
@@ -23,7 +40,35 @@ public class ApiHandler : MonoBehaviour
     public string localSubject = "";
     public string localCategory = "";
 
-
+    IEnumerator GetBotStatus()
+    {
+        using (UnityWebRequest req = UnityWebRequest.Get(api_endpoint))
+        {
+            yield return req.Send();
+            while (!req.isDone)
+                yield return null;
+            byte[] result = req.downloadHandler.data;
+            string weatherJSON = System.Text.Encoding.Default.GetString(result);
+            BotStatus info = JsonUtility.FromJson<BotStatus>(weatherJSON);
+            Debug.Log(info);
+            processApiResponseValues(
+                    info.lastRoundStatus,
+                        info.lastMessageTime,
+                        info.lastMessage,
+                        info.partialMessage,
+                        info.botLoopStep,
+                        info.subject,
+                        info.category);
+            StartCoroutine(GetBotStatus());
+        }
+    }
+    IEnumerator WaitForSplash()
+    {
+        yield return new WaitForSeconds(2);
+        while (audioLoader.audioSource.isPlaying)
+            yield return null;
+        StartCoroutine(GetBotStatus());
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -32,13 +77,13 @@ public class ApiHandler : MonoBehaviour
         pageLoader = GetComponent<DynamicPageLoader>();
         emissionController = GetComponent<PulseEmissionFromSound>();
         audioLoader = GetComponent<LoadDynamicAudio>();
+        StartCoroutine(WaitForSplash());
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void processApiResponseValues(
