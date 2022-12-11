c57738dd3890679dcb82ffea541ae3fe29568dbf
tpoff6 <tpoff6@gatech.edu>
2022-12-11 00:38:39 -0600

    added empty animation script for the avatar, will populate once model is created but needed a mock version to start api mock integration.

---
+++ AvatarDisplay/Assets/Assets/Scripts/AvatarAnimatorScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvatarAnimatorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updateState(string newState)
+    {
+        // TODO: once avatar model is built and animated. 
+    }
+}
