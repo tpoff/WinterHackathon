22d9d64594bb148e0774462fedc795587586fdf8
tpoff6 <tpoff6@gatech.edu>
2022-12-11 02:13:58 -0600

    stashing changes for merge.

--- AvatarDisplay/Assets/Assets/Scripts/ApiHandler.cs
+++ AvatarDisplay/Assets/Assets/Scripts/ApiHandler.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class ApiHandler : MonoBehaviour
 {
+    public string generatedPagePath = "";
+    public string generatedAudioPath = "";
+    public string initialPagePath = "";
+    public string initialAudioPath = "";
+    public AvatarAnimatorScript animatorScript;
+    public DynamicPageLoader pageLoader;
+    public PulseEmissionFromSound emissionController;
+    public LoadDynamicAudio audioLoader;
+
+    protected bool recievedFirstApiCall = false;
+
+    public string localLastRoundStatus = "NOT_RUN";
+    public float localLastMessageTime = 0;
+    public string localLastMessage = "";
+    public string localPartialMessage = "";
+    public string localLastBotLoopStep = "SETUP";
+    public string localSubject = "";
+    public string localCategory = "";
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animatorScript = GetComponent<AvatarAnimatorScript>();
+        pageLoader = GetComponent<DynamicPageLoader>();
+        emissionController = GetComponent<PulseEmissionFromSound>();
+        audioLoader = GetComponent<LoadDynamicAudio>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void processApiResponseValues(
+            string lastRoundStatus,
+            float lastMessageTime,
+            string lastMessage,
+            string partialMessage,
+            string botLoopStep,
+            string subject,
+            string category
+        )
+    {
+        localCategory = category;
+        localLastBotLoopStep = botLoopStep;
+        localLastMessage = lastMessage;
+        localLastRoundStatus = lastRoundStatus;
+        localPartialMessage = partialMessage;
+        localCategory = category;
+        localSubject = subject;
+
+
+        if (!recievedFirstApiCall && botLoopStep == "STANDBY")
+        {
+            animatorScript.updateState(localLastBotLoopStep);
+            recievedFirstApiCall = true;
+            pageLoader.loadPage(initialPagePath);
+            audioLoader.loadAudioClip(initialAudioPath);
+        }
+        else
+        {
+            animatorScript.updateState(localLastBotLoopStep);
+
+            if (localLastMessageTime != lastMessageTime)
+            {
+                pageLoader.loadPage(generatedPagePath);
+                audioLoader.loadAudioClip(generatedAudioPath);
+            }
+        }
+        localLastMessageTime = lastMessageTime;
+
     }
 }
